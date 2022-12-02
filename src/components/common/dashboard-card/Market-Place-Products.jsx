40f4f2e05842ac7a4c7853431f03b59096628bd9
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2022-12-03 00:35:37 +0100

    setLoading-time-out

--- src/components/common/dashboard-card/Market-Place-Products.jsx
+++ src/components/common/dashboard-card/Market-Place-Products.jsx
@@ -11,12 +11,19 @@ import { icons } from "../../../constant/icon";
 
 
 const MarketPlaceProduct = () => {
-    const { access_token } = useSelector(state => state.auth);
-    const [isLoading, setIsLoading ] = useState(false);
+    const { access_token, } = useSelector(state => state.auth);
+    const [isLoading, setIsLoading] = useState(false);
     const [products, setProducts] = useState([]);
     useEffect(() => {
+
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
+
         (async () => {
-            setIsLoading(true);
+
+
             try {
                 const config = {
                     headers: {
@@ -24,18 +31,11 @@ const MarketPlaceProduct = () => {
                     },
                 };
                 const { data } = await axios.get('https://artsec-service-cjfd8.ondigitalocean.app/api/v1/get_products', config);
-                setIsLoading(false);
-                setProducts(data.data.data)
+                setProducts(data.data.data);
+
             } catch (err) {
-                setIsLoading(false);
                 toast.error(err.message, {
                     position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
                 });
             }
 
@@ -46,53 +46,53 @@ const MarketPlaceProduct = () => {
 
     return (
         <>
-           {isLoading ? <DataLoader />: 
-            <>
-                 {
-                products.map((item, index) => (
-                    <div key={index} className="bg-white shadow">
-                        <img src={'https://artsec-service-cjfd8.ondigitalocean.app/api/v1/get_path?img_path=' + item.img} alt="" className="w-full h-48" />
-                        <div className="p-2">
-                            <div className="flex justify-between items-center">
-                                <div>
-                                    <p className='text-[18px] font-semibold'>{item.name}</p>
-                                    <p className='text-[15px] font-normal text-[#1B1F28]'>{item?.product_by}</p>
-                                </div>
-                                <MartketButton
-                                    className="flex justify-between items-center p-2 bg-[#D1FAEC] border border-[#36D39E] rounded-[10px] cursor-pointer text-[#1B1F28]"
-                                    title={item?.rate}
-                                    icon={<icons.ArtSecPlaceBid className='mr-2' />}
-                                />
-                            </div>
-                            <div className="flex justify-between items-center py-2">
-                                <div>
-                                    <p className='text-[15px] font-normal text-[#606C84]'>Sale Price</p>
-                                    <p className='text-[18px] font-semibold'>$ {item.sale_price}</p>
-                                </div>
-                                <div>
-                                    <p className='text-[15px] font-normal text-[#606C84]'>Purchase Price</p>
-                                    <p className='text-[18px] font-semibold'>$ {item.purchase_price}</p>
-                                </div>
-                            </div>
+            {isLoading ? <DataLoader /> :
+                <>
+                    {
+                        products.map((item, index) => (
+                            <div key={index} className="bg-white shadow">
+                                <img src={'https://artsec-service-cjfd8.ondigitalocean.app/api/v1/get_path?img_path=' + item.img} alt="" className="w-full h-48" />
+                                <div className="p-2">
+                                    <div className="flex justify-between items-center">
+                                        <div>
+                                            <p className='text-[18px] font-semibold'>{item.name}</p>
+                                            <p className='text-[15px] font-normal text-[#1B1F28]'>{item?.product_by}</p>
+                                        </div>
+                                        <MartketButton
+                                            className="flex justify-between items-center p-2 bg-[#D1FAEC] border border-[#36D39E] rounded-[10px] cursor-pointer text-[#1B1F28]"
+                                            title={item?.rate}
+                                            icon={<icons.ArtSecPlaceBid className='mr-2' />}
+                                        />
+                                    </div>
+                                    <div className="flex justify-between items-center py-2">
+                                        <div>
+                                            <p className='text-[15px] font-normal text-[#606C84]'>Sale Price</p>
+                                            <p className='text-[18px] font-semibold'>$ {item.sale_price}</p>
+                                        </div>
+                                        <div>
+                                            <p className='text-[15px] font-normal text-[#606C84]'>Purchase Price</p>
+                                            <p className='text-[18px] font-semibold'>$ {item.purchase_price}</p>
+                                        </div>
+                                    </div>
 
-                            <div className="flex justify-between items-center py-2">
-                                <Link to={"/dashboard/artwork-details/" + item.id}>
-                                    <MartketButton
-                                        className="flex justify-between items-center px-8 py-2 border border-[#4B006E] rounded-md cursor-pointer app-color hover:bg-[#4b006e] hover:text-white"
-                                        title='Place a Bid'
-                                        icon={<icons.ArtSecPlaceBid className='mr-2' />}
-                                    />
-                                </Link>
-                                <div>
-                                    {<icons.ArtSecFavourite />}
+                                    <div className="flex justify-between items-center py-2">
+                                        <Link to={"/dashboard/artwork-details/" + item.id}>
+                                            <MartketButton
+                                                className="flex justify-between items-center px-8 py-2 border border-[#4B006E] rounded-md cursor-pointer app-color hover:bg-[#4b006e] hover:text-white"
+                                                title='Place a Bid'
+                                                icon={<icons.ArtSecPlaceBid className='mr-2' />}
+                                            />
+                                        </Link>
+                                        <div>
+                                            {<icons.ArtSecFavourite />}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                ))
+                        ))
+                    }
+                </>
             }
-            </>
-           }
         </>
     )
 }
