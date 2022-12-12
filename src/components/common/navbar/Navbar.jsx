a3def89cbb557a88070d683b367d60fd6da5165b
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2022-12-12 12:12:20 +0100

    delopy-bug-fixed

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -11,6 +11,7 @@ import { logout } from '../../../redux/auth.slice';
 
 
 const NavButton = ({ customFunc, icon, color, dotColor }) => (
+  // navbar button
   <>
     <button
       type="button"
@@ -41,7 +42,7 @@ const Navbar = () => {
     handleResize();
 
     return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  },[]);
 
   useEffect(() => {
     if (screenSize <= 900) {
